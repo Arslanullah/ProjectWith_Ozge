fd86994cbe3a113da00b2ff54a2656888178a673
Arslanullah <114203538+Arslanullah@users.noreply.github.com>
2023-01-04 15:34:02 -0600

    My first Code with Ozge

---
+++ src/HelloWorld.java
@@ -0,0 +1,17 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+
+        System.out.println("hello world ");
+        // i made some changes for the project Ozge and now im gonna push the Code
+        // what is Git?
+        //   Ans) it is version control?   ---------git
+        // simply answer to share my codes and work flow
+        //arhum 
+        ////what is repository?   == >  Github
+           //  and online reposotory where u save files..(bank ,cloud  yxz)Storage
+
+
+
+
+    }
+}
